9134770e1c14f4194324c89357996def9624d25f
Sharjith a <146409470+sharjicodes@users.noreply.github.com>
2026-02-09 12:13:13 +0530

    Update support content via CMS

---
+++ content/support.ts
@@ -0,0 +1,18 @@
+
+const content = {
+  "title": "support",
+  "description": "choose any support from team",
+  "sections": [
+    {
+      "title": "Our Mission",
+      "description": "To demonstrate that you don't always need a database."
+    },
+    {
+      "title": "Our Vision",
+      "description": "Content as Code."
+    }
+  ]
+};
+
+export type PageContent = typeof content;
+export default content;
